cdaa88053e40f05d01e8aa9d23c66edcc6497857
Lars Nielsen <lnc13.lars@gmail.com>
2016-01-15 16:33:16 +0100

    Initial Commit

---
+++ irisPause/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  irisPause
+//
+//  Created by Lars Nielsen on 15/01/16.
+//  Copyright © 2016 Lars Nielsen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
